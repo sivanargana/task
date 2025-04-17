d1626ce2fb2912f1fb30b8543c5f09ce5dca6167
sivanargana <siva.nargana66@gmail.com>
2025-04-18 00:37:32 +0530

    new changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,31 @@
 let sidebarElement = document.querySelector("[data-layout='sidebar']");
 let sidebarToggle = document.querySelector("[data-layout='toggle']");
-isSidebarOpen = true;
-
-const setSidebar = ()=>{
-    if(!isSidebarOpen){
-        isSidebarOpen = true;
-        sidebarElement.classList.add("open")
-    }else{
-        isSidebarOpen = false;
-        sidebarElement.classList.remove("open")
+if (sidebarElement) {
+    isSidebarOpen = true;
+    const setSidebar = () => {
+        if (!isSidebarOpen) {
+            isSidebarOpen = true;
+            sidebarElement.classList.add("open")
+        } else {
+            isSidebarOpen = false;
+            sidebarElement.classList.remove("open")
+        }
     }
-}
-
-
-setSidebar();
- 
-
-
-sidebarToggle.addEventListener("click",()=>{
     setSidebar();
-})
+    sidebarToggle.addEventListener("click", () => {
+        setSidebar();
+    })
+}
+function logout(e) {
+    Swal.fire({
+        html: `<div class="tw-font-bold">Are you sure you want to logout?</div>`,
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        confirmButtonColor: "var(--primary)",
+        cancelButtonText: "No"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location = "/login.html"
+        }
+    });
+}
